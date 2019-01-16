313b69f7b51be746ea733d71e6493177c982c512
Angel <aleangrodriguez123@gmail.com>
2019-01-15 21:32:24 -0500

    Initial Commit

---
+++ src/InfiniteScroll.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    ref="container"
+    @scroll="scrollHandler"
+  >
+    <slot/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "InfiniteScroll",
+    methods: {
+      scrollHandler() {
+        if (this.isAtTheBottom()) {
+            this.$emit("scrollEnd");
+        }
+      },
+      isAtTheBottom() {
+          const $container = this.$refs.container;
+          // Use getBoundingClientRect for get height with decimal part
+          const {height} = $container.getBoundingClientRect();
+          const {scrollHeight, scrollTop} = $container;
+
+          // Using truncate function for avoid problems with extra pixels in some list elements 
+          // and get a more accurate difference
+
+          const remainingHeight = Math.trunc(scrollHeight - scrollTop);
+          const totalHeight = Math.trunc(height);
+
+          // TODO: Get a method for obtain the exact pixels amount for element
+          // Bug: ScrollHeight is retreinving a exceed pixel, it shouldn't according to https://mzl.la/2EKZ3s2
+          // Fix: Added a range for avoid exceeding pixels on scroll calculation (NOT DEFINITIVED SOLUTION)
+
+          const range = 5;
+          return remainingHeight < totalHeight + range && remainingHeight > totalHeight - range;
+      }
+    }
+  }
+</script>
+
